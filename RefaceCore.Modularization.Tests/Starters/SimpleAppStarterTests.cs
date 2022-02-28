75074f9ae0bff102b5f366def57a0619f50076bc
Felix <116971263@qq.com>
2022-02-28 08:45:12 +0800

    feat : add unit test

---
+++ RefaceCore.Modularization.Tests/Starters/SimpleAppStarterTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RefaceCore.Modularization.Starters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RefaceCore.Modularization.Starters.Tests
+{
+    [TestClass()]
+    public class SimpleAppStarterTests
+    {
+        [TestMethod()]
+        public void StartTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
